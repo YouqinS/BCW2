4f228e61f7a2d02059890504b7a3b6056183b920
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-11-05 22:59:33 +0200

    tasks done

--- js/main-a.js
+++ js/main-a.js
@@ -1,2 +1,62 @@
 // Create function 'showImages' which
 // adds the loaded HTML content to <ul> element
+
+
+//response.text().then(function (text) {
+//   // do something with the text response
+// });
+
+const showImages = (html) =>{
+  document.querySelector('ul').innerHTML = html;
+};
+
+fetch('images.html').then((response) =>{  //with brackets: (response)=>
+  return response.text();
+}).then((html) =>{
+  showImages(html);
+});
+
+
+
+//XMLHttpRequest
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//       document.getElementById("demo").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "images.html", true);
+//   xhttp.send();
+// }
+
+
+// //document.getElementById("btnGetFile").addEventListener("click", function(){
+//   const xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//       document.getElementsByTagName('ul')[0].innerHTML = this.responseText;
+//       //document.getElementById("images").innerHTML = this.responseText;
+//       //document.getElementById('file').innerHTML = this.responseText;
+//      }
+//   };
+//   xhttp.open("GET", "images.html", true);
+//   xhttp.send();
+//});
+
+
+//document.getElementById("myBtn").addEventListener("click", myFunction);
+//
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+
+//document.getElementsByTagName('ul').innerHTML = this.responseText;
+//document.getElementsByClassName('primary-color').innerHTML = this.responseText;
+
+//var func = x => x * x;
+// // concise body syntax, implied "return"
+//
+// var func = (x, y) => { return x + y; };
+// // with block body, explicit "return" needed
--- js/main-b.js
+++ js/main-b.js
@@ -13,3 +13,40 @@
 </li>
 */
 // After the loop print the HTML into <ul> element using innerHTML.
+
+
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Body/json
+//response.json().then(function(data) {
+//   // do something with your data
+// });
+
+const showImages = (images) =>{
+  // images.forEach((image)=>{
+  //   document.querySelector('ul').innerHTML += '....';
+  // });
+
+  //or
+  // for(let i = 0; i< images.length; i++){
+  //   console.log(images[i]);
+  // }
+
+  //or
+   document.querySelector('ul').innerHTML = images.map((image)=>{
+     return `<li> <figure>
+                      <a href="img/original/${image.mediaUrl}"><img src="img/thumbs/${image.mediaThumb}"></a>
+                      <figcaption>
+                      <h3>${image.mediaTitle}</h3>
+                      </figcaption>
+                   </figure>
+            </li>`;
+   }).join("");
+};
+
+fetch('images.json').then((response) =>{  //with brackets: (response)=>
+  return response.json();
+}).then((json) =>{
+  showImages(json);
+});
